af861f41c4d31036828b38403bbab34027d2727c
Franz Vilela Campero <franz_cv456@hotmail.com>
2020-04-14 19:28:08 -0400

    jugar1

--- app/src/main/java/com/app/examen_juego/JuegoPresenter.kt
+++ app/src/main/java/com/app/examen_juego/JuegoPresenter.kt
@@ -4,7 +4,8 @@ import android.content.Context
 
 class JuegoPresenter(val view: IContractJuego.View, val context: Context) : IContractJuego.Presenter {
 
-    lateinit var posiciones: MutableList<Int>
+    lateinit var posicionesJ1: MutableList<Int>
+    lateinit var posicionesJ2: MutableList<Int>
     var posicionesLibres:MutableList<Int> = arrayListOf(1,2,3,4,5,6,7,8,9)
     override fun jugar(pos: Int) {
         turnoJugador1(pos)
@@ -12,100 +13,100 @@ class JuegoPresenter(val view: IContractJuego.View, val context: Context) : ICon
         turnoJugador2(randomNumber)
     }
 
-    private fun turnoJugador2(pos: Int){
+    private fun turnoJugador1(pos: Int){
         if (pos == 1 && posLibre(1)) {
             view.pos1Selected(2)
-            posiciones.add(1)
+            posicionesJ1.add(1)
             posicionesLibres.remove(1)
         }
         if (pos == 2 && posLibre(2)) {
             view.pos2Selected(2)
-            posiciones.add(2)
+            posicionesJ1.add(2)
             posicionesLibres.remove(2)
         }
         if (pos == 3 && posLibre(3)) {
             view.pos3Selected(2)
-            posiciones.add(3)
+            posicionesJ1.add(3)
             posicionesLibres.remove(3)
         }
 
         if (pos == 4 && posLibre(4)) {
             view.pos4Selected(2)
-            posiciones.add(4)
+            posicionesJ1.add(4)
             posicionesLibres.remove(4)
         }
         if (pos == 5 && posLibre(7)) {
             view.pos5Selected(2)
-            posiciones.add(5)
+            posicionesJ1.add(5)
             posicionesLibres.remove(5)
         }
         if (pos == 6 && posLibre(6)) {
             view.pos6Selected(2)
-            posiciones.add(6)
+            posicionesJ1.add(6)
             posicionesLibres.remove(6)
         }
         if (pos == 7 && posLibre(7)) {
             view.pos7Selected(2)
-            posiciones.add(7)
+            posicionesJ1.add(7)
             posicionesLibres.remove(7)
         }
         if (pos == 8 && posLibre(8)) {
             view.pos8Selected(2)
-            posiciones.add(8)
+            posicionesJ1.add(8)
             posicionesLibres.remove(8)
         }
         if (pos == 9 && posLibre(9)) {
             view.pos9Selected(2)
-            posiciones.add(9)
+            posicionesJ1.add(9)
             posicionesLibres.remove(9)
         }
     }
 
-    private fun turnoJugador1(pos: Int){
+    private fun turnoJugador2(pos: Int){
         if (pos == 1 && posLibre(1)) {
             view.pos1Selected(1)
-            posiciones.add(1)
+            posicionesJ2.add(1)
             posicionesLibres.remove(1)
         }
         if (pos == 2 && posLibre(2)) {
             view.pos2Selected(1)
-            posiciones.add(2)
+            posicionesJ2.add(2)
             posicionesLibres.remove(2)
         }
         if (pos == 3 && posLibre(3)) {
             view.pos3Selected(1)
-            posiciones.add(3)
+            posicionesJ2.add(3)
             posicionesLibres.remove(3)
         }
 
         if (pos == 4 && posLibre(4)) {
             view.pos4Selected(1)
-            posiciones.add(4)
+            posicionesJ2.add(4)
             posicionesLibres.remove(4)
         }
         if (pos == 5 && posLibre(7)) {
             view.pos5Selected(1)
-            posiciones.add(5)
+            posicionesJ2.add(5)
             posicionesLibres.remove(5)
         }
         if (pos == 6 && posLibre(6)) {
             view.pos6Selected(1)
-            posiciones.add(6)
+            posicionesJ2.add(6)
             posicionesLibres.remove(6)
         }
         if (pos == 7 && posLibre(7)) {
             view.pos7Selected(1)
-            posiciones.add(7)
+            posicionesJ2.add(7)
             posicionesLibres.remove(7)
         }
         if (pos == 8 && posLibre(8)) {
             view.pos8Selected(1)
-            posiciones.add(8)
+            posicionesJ2.add(8)
             posicionesLibres.remove(8)
         }
         if (pos == 9 && posLibre(9)) {
             view.pos9Selected(1)
-            posiciones.add(9)
+            posicionesJ2.add(9)
             posicionesLibres.remove(9)
         }
     }
--- app/src/main/java/com/app/examen_juego/MainActivity.kt
+++ app/src/main/java/com/app/examen_juego/MainActivity.kt
@@ -3,14 +3,45 @@ package com.app.examen_juego
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),IContractJuego.View {
 
-    lateinit var loginPresenter: IContractJuego.Presenter
+    lateinit var juegoPresenter: IContractJuego.Presenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickButton(view: View){
+        if (view.id == btn_pos00.id){
+            juegoPresenter.jugar(1)
+        }
+        if (view.id == btn_pos01.id){
+            juegoPresenter.jugar(2)
+        }
+        if (view.id == btn_pos02.id){
+            juegoPresenter.jugar(3)
+        }
+        if (view.id == btn_pos10.id){
+            juegoPresenter.jugar(4)
+        }
+        if (view.id == btn_pos11.id){
+            juegoPresenter.jugar(5)
+        }
+        if (view.id == btn_pos12.id){
+            juegoPresenter.jugar(6)
+        }
+        if (view.id == btn_pos20.id){
+            juegoPresenter.jugar(7)
+        }
+        if (view.id == btn_pos21.id){
+            juegoPresenter.jugar(8)
+        }
+        if (view.id == btn_pos22.id){
+            juegoPresenter.jugar(9)
+        }
     }
 
     override fun showWinMessage() {
@@ -26,8 +57,6 @@ class MainActivity : AppCompatActivity(),IContractJuego.View {
         }else {
             btn_pos00.setBackgroundColor(resources.getColor(R.color.j2))
         }
-
-
     }
 
     override fun pos2Selected(numeroJugador: Int) {
